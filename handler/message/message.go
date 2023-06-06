1d824a06790a852d5a00144e320fa6f06f59573b
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-06-06 15:36:08 +0900

    ruleの階層を分割

--- handler/message/message.go
+++ handler/message/message.go
@@ -3,6 +3,7 @@ package message
 import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/techstart35/the-anarchy-bot/errors"
+	"github.com/techstart35/the-anarchy-bot/handler/message/rule"
 )
 
 const CMD_Send_Rule = "!an-rule"
@@ -11,7 +12,7 @@ const CMD_Send_Rule = "!an-rule"
 func MessageCreateHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	switch m.Content {
 	case CMD_Send_Rule:
-		if err := SendRule(s, m); err != nil {
+		if err := rule.SendRule(s, m); err != nil {
 			errors.SendErrMsg(s, errors.NewError("ルールを送信できません", err))
 		}
 	}
--- handler/message/rule/rule.go
+++ handler/message/rule/rule.go
@@ -1,4 +1,4 @@
-package message
+package rule
 
 import (
 	"github.com/bwmarrin/discordgo"
