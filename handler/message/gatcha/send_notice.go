3bce1b4a04a03cd882ca0b4abccf1b40af74a066
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-06-17 01:12:14 +0900

    ガチャの通知先を変更

--- handler/message/gatcha/send_notice.go
+++ handler/message/gatcha/send_notice.go
@@ -5,31 +5,18 @@ import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/techstart35/the-anarchy-bot/errors"
 	"github.com/techstart35/the-anarchy-bot/internal"
-	"strings"
 )
 
 // 毎日の通知メッセージを送信します
 func SendNotice(s *discordgo.Session) error {
-	// 前回のメッセージを削除します
-	messages, err := s.ChannelMessages(internal.ChannelID().GATCHA, 1, "", "", "")
-	if err != nil {
-		return errors.NewError("最新メッセージを取得できません", err)
-	}
-
-	latestMessage := messages[0]
-	if strings.Contains(latestMessage.Content, "おはようございます") {
-		if err = s.ChannelMessageDelete(latestMessage.ChannelID, latestMessage.ID); err != nil {
-			return errors.NewError("前回の通知メッセージを削除できません", err)
-		}
-	}
-
 	// 通知を送信します
 	msg := fmt.Sprintf(
-		"<@&%s>\nおはようございます！今日もガチャを回してみよう🎫",
+		"<@&%s>\nおはようございます！今日も<#%s>を回してみよう！！",
 		internal.RoleID().GATCHA_NOTICE,
+		internal.ChannelID().GATCHA,
 	)
 
-	_, err = s.ChannelMessageSend(internal.ChannelID().GATCHA, msg)
+	_, err := s.ChannelMessageSend(internal.ChannelID().CHAT, msg)
 	if err != nil {
 		return errors.NewError("通知メッセージを送信できません", err)
 	}
