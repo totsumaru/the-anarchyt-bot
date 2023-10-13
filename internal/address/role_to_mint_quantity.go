123fcb68f7bb230bd5351f157c7d6d8cdbf2c20b
totsuka <techstart35@gmail.com>
2023-10-13 12:40:17 +0900

    fix

--- internal/address/role_to_mint_quantity.go
+++ internal/address/role_to_mint_quantity.go
@@ -4,7 +4,14 @@ import "github.com/techstart35/the-anarchy-bot/internal"
 
 // ロールに対するMint上限です
 var RoleMaxMintMap = map[string]int{
-	internal.RoleID().AL: 2,
+	internal.RoleID().AL:            2,
+	internal.RoleID().BRONZE:        1,
+	internal.RoleID().SILVER:        1,
+	internal.RoleID().GOLD:          1,
+	internal.RoleID().PLATINUM:      1,
+	internal.RoleID().DIAMOND:       1,
+	internal.RoleID().CRAZY:         1,
+	internal.RoleID().CHAINSAW_CLUB: 1,
 }
 
 // Mint上限を取得します
@@ -14,6 +21,10 @@ func MaxMintQuantity(roles []string) int {
 		if quantity, ok := RoleMaxMintMap[role]; ok {
 			res += quantity
 		}
+	}
+
+	if res >= 4 {
+		return 4
 	}
 
 	return res
