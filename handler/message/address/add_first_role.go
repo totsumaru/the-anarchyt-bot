bf2b76d86e4ebc28394d862be8e8cb410dab3ce4
totsuka <techstart35@gmail.com>
2023-10-25 10:15:21 +0900

    fix

--- handler/message/address/add_first_role.go
+++ handler/message/address/add_first_role.go
@@ -1,18 +1,33 @@
 package address
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
+	"github.com/techstart35/the-anarchy-bot/errors"
 	"github.com/techstart35/the-anarchy-bot/internal"
 )
 
 // 第一弾で登録した人にロールを付与します
 func AddFirstWalletSubmittedRole(s *discordgo.Session, m *discordgo.MessageCreate) error {
+	// Totsumaru以外は実行できません
+	if m.Author.ID != internal.UserID().TOTSUMARU {
+		return nil
+	}
+
+	if _, err := s.ChannelMessageSend(m.ChannelID, "開始します"); err != nil {
+		return errors.NewError("メッセージを送信できません")
+	}
+
 	// 指定されたギルドのメンバーを取得します。
 	// 1000は1回のAPIリクエストで取得するメンバーの最大数です。必要に応じて調整してください。
 	members, err := s.GuildMembers(m.GuildID, "", 1000)
 	if err != nil {
 		return err
 	}
+
+	// 付与済みカウント
+	count := 0
 
 	// 各メンバーを調査して、ロールAを持っているかどうかを確認します。
 	for _, member := range members {
@@ -34,6 +49,11 @@ func AddFirstWalletSubmittedRole(s *discordgo.Session, m *discordgo.MessageCreat
 			); err != nil {
 				return err
 			}
+			count++
+		}
+
+		if _, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%d人にロールを付与しました。", count)); err != nil {
+			return errors.NewError("メッセージを送信できません")
 		}
 	}
 
